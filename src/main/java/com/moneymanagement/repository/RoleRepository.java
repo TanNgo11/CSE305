b904b8a1c3dde5dd82ae06fb4d7039d601cb033e
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2023-11-09 23:13:42 +0700

    hhehe
    fix security

--- src/main/java/com/moneymanagement/repository/RoleRepository.java
+++ src/main/java/com/moneymanagement/repository/RoleRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.moneymanagement.entity.RoleEntity;
 
+
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
 	RoleEntity findByName(String name);
 }
